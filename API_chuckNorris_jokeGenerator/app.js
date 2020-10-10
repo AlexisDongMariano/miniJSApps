5c542fbdf982f9d6d99516ff77dee838f9624c5a
unknown <alexisdongmariano@gmail.com>
2020-10-11 01:04:15 +0800

    added jokeGenerator

---
+++ API_chuckNorris_jokeGenerator/app.js
@@ -0,0 +1,36 @@
+document.querySelector('.get-jokes').addEventListener('click', getJokes);
+
+function getJokes(e) {
+  const number = document.querySelector('input[id="number"]').value;
+
+
+  xhr = new XMLHttpRequest();
+  xhr.open('GET', `http://api.icndb.com/jokes/random/${number}`, true);
+
+  xhr.onload = function () {
+    console.log(this.readyState);
+    if (this.status === 200) {
+      const response = JSON.parse(this.responseText);
+      console.log(response);
+
+      let output = '';
+      if (response.type === 'success') {
+        response.value.forEach(function (item) {
+          output += `
+            <li>${item.joke}</li>
+          `;
+        })
+
+      }
+      else {
+        output += `<li>something went wrong<\li>`;
+      }
+
+      document.querySelector('.jokes').innerHTML = output;
+    }
+  }
+
+  xhr.send();
+
+  e.preventDefault();
+}
