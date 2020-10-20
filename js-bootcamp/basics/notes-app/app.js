1df11091f7894f5b38e584acacb54bb44c4a3555
unknown <alexisdongmariano@gmail.com>
2020-10-21 02:20:31 +0800

    modified notes and todo app

---
+++ js-bootcamp/basics/notes-app/app.js
@@ -0,0 +1,44 @@
+const notes = [{
+    title: 'my next trip',
+    body: 'I would like to go to Spain'
+}, {
+    title: 'Habits to work on',
+    body: 'Exercise. Eating a bit better.'
+}, {
+    title: 'Office modification',
+    body: 'Get a new seat'
+}]
+
+const filters = {
+    searchText: ''
+};
+
+const renderNotes = (notes, filters) => {
+    const filteredNotes = notes.filter(note => {
+        return note.title.toLowerCase().includes(filters.searchText.toLowerCase());
+    });
+
+    div_notes = document.querySelector('#notes');
+    div_notes.innerHTML = '';
+
+    filteredNotes.forEach(note => {
+        h4_new = document.createElement('h4');
+        h4_new.textContent = note.title;
+        div_notes.appendChild(h4_new);
+    })
+}
+
+renderNotes(notes, filters);
+
+document.querySelector('#search-text').addEventListener('input', (e) => {
+    filters.searchText = e.target.value;
+    renderNotes(notes, filters);
+})
+
+document.querySelector('#add-todo').addEventListener('click', (e) => {
+    console.log(`I'm adding a new todo.`);
+})
+
+document.querySelector('#input-todo').addEventListener('input', (e) => {
+    console.log(e.target.value);
+})
---
+++ js-bootcamp/basics/todo-app/app.js
@@ -0,0 +1,72 @@
+const todos = [
+    {
+        text: 'Order Cat food',
+        completed: false
+    },
+    {
+        text: 'Clean kitchen',
+        completed: true
+    },
+    {
+        text: 'Buy food',
+        completed: true
+    },
+    {
+        text: 'Do work',
+        completed: false
+    },
+    {
+        text: 'Exercise',
+        completed: true
+    }
+]
+
+// You have x todos left (p element)
+// Add a p element for each above (use text value)
+// 1. setup a div contain for todos
+// 2. setup filters (searchText) and wire up a new filter input to change it
+// 3. Create a renderTodos function to render and rerender the latest filtered data
+
+// UI Components
+input_search = document.querySelector('#search-text');
+div_todos = document.querySelector('#todos');
+
+// Event listeners
+input_search.addEventListener('input', (e) => {
+    filters.searchText = e.target.value;
+    console.log(filters.searchText);
+    filterText(todos, filters);
+});
+
+const filters = {
+    searchText: ''
+};
+
+const todosLeftCount = todos.filter(todo => {
+    return !todo.completed;
+});
+
+p_todosLeft = document.createElement('h2');
+p_todosLeft.textContent = `You have ${todosLeftCount.length} todos left`;
+document.body.appendChild(p_todosLeft);
+
+const displayTodos = todos => {
+    div_todos.innerHTML = '';
+    todos.forEach(todo => {
+        let p_todo = document.createElement('p');
+        p_todo.textContent = todo.text;
+        div_todos.appendChild(p_todo);
+    })
+}
+
+displayTodos(todos);
+
+function filterText(todos, filters) {
+    filteredTodos = todos.filter(todo => {
+        return todo.text.toLowerCase().includes(filters.searchText.toLowerCase());
+    })
+
+    displayTodos(filteredTodos);
+
+}
+
